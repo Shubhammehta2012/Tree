80ddd25540b532676a9e564814a0c5abdab0a2f1
Shubham Mehta <31146862+Shubhammehta2012@users.noreply.github.com>
2018-08-21 02:53:31 -0700

    Update RootToLeafPaths.py

--- RootToLeafPaths.py
+++ RootToLeafPaths.py
@@ -1,9 +1,3 @@
-# Author: OMKAR PATHAK
-
-# Use a path array path[] to store current root to leaf path. Traverse from root to all leaves in top-down fashion.
-# While traversing, store data of all nodes in current path in array path[]. When we reach a leaf node, print the path
-# array.
-
 class Node(object):
     def __init__(self, data = None):
         self.left = None
